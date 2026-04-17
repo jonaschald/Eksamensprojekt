98a069d33f3cff42ae383131f0d92632b7c55bdb
Jonas <jonhal01@easv365.dk>
2026-04-17 12:12:15 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.eksamensprojekt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.eksamensprojekt to javafx.fxml;
+    exports com.example.eksamensprojekt;
+}
