37f46f48219cbc8ad6a6c15c3fcbf770b3030d7a
Jonas <jonhal01@easv365.dk>
2026-04-28 10:19:10 +0200

    Fix Om Os and add Om Os controller

---
+++ src/main/java/com/example/eksamensprojekt/OmOsController.java
@@ -0,0 +1,48 @@
+package com.example.eksamensprojekt;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class OmOsController {
+
+    @FXML
+    void adminKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void besøgKunsthallensHjemmesideKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void favoritterKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void omOsKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void omSamlingenKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void temaerKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void undervisningKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void watanabeSamlingenKnap(MouseEvent event) {
+
+    }
+
+}
