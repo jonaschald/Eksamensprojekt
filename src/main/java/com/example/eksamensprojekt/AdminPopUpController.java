0c15ae33249c4e7e7395e2ae7c2e35f757cde954
emmRas01 <emmras01@easv365.dk>
2026-04-28 12:46:36 +0200

    Add Emmas FXMLs

---
+++ src/main/java/com/example/eksamensprojekt/AdminPopUpController.java
@@ -0,0 +1,46 @@
+package com.example.eksamensprojekt;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class AdminPopUpController {
+
+    @FXML
+    private Label InfoLabel;
+
+    @FXML
+    private Label besktivelseLabel;
+
+    @FXML
+    private ImageView popupBillede;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Label årstalLabel;
+
+    @FXML
+    void forrigeBilledeKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void næsteBilledeKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void redigerKunstværk(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tilbageTilOversigtKnap(MouseEvent event) {
+
+    }
+
+}
