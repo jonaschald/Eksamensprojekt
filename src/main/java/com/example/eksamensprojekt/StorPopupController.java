113c16310cc9f60aa2411739104d227ed16c68ce
Majken <majros01@easv365.dk>
2026-04-28 10:00:36 +0200

    tilføje FXML og controller til AdminForside, StorPopup, AdminUndervisning og Undervisning

---
+++ src/main/java/com/example/eksamensprojekt/StorPopupController.java
@@ -0,0 +1,31 @@
+package com.example.eksamensprojekt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class StorPopupController {
+
+    @FXML
+    private ImageView storPopupBillede;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    void forrigeBilledeKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void næsteBilledeKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tilbageTilOversigtKnap(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/eksamensprojekt/UndervisningController.java
@@ -0,0 +1,61 @@
+package com.example.eksamensprojekt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class UndervisningController {
+
+    @FXML
+    private ListView<?> indskoling;
+
+    @FXML
+    private ListView<?> konfirmation;
+
+    @FXML
+    private ListView<?> mellemtrin;
+
+    @FXML
+    private ListView<?> udskoling;
+
+    @FXML
+    void adminKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void besøgKunsthallensHjemmesideKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void favoritterKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void omOsKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void omSamlingenKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void temaerKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void undervisningKnap(MouseEvent event) {
+
+    }
+
+    @FXML
+    void watanabeSamlingenKnap(MouseEvent event) {
+
+    }
+
+}
